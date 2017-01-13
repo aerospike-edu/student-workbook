d53d8fa214ac0d3445c7edf6c8b10604092bde55
Piyush Gupta <piyush@Administrators-MacBook-Pro-4.local>
2017-01-12 20:43:12 -0800

    AS102 bm scripts

--- AS102/ex3_bmReadUpdateData.sh
+++ AS102/ex3_bmReadUpdateData.sh
@@ -3,4 +3,4 @@
 # Load test data using benchmark
 # -
 cd ~/packages/aerospike/aerospike-client-java-*/benchmarks
-./run_benchmarks -h 127.0.0.1 -p 3000 -n test -s testset -k 100000 -latency "7,1" -o S:100 -w RU,95 -z 8
+./run_benchmarks -h 127.0.0.1 -p 3000 -n test -s testset -k 100000 -latency "7,1" -S 1 -o S:100 -w RU,95 -z 8
