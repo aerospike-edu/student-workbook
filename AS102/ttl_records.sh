201c25eca39260b5bf34151e4229a11aef23dc8c
Piyush Gupta <piyush@Administrators-MacBook-Pro-4.local>
2017-01-13 01:14:56 -0800

    AS102 ttl script

--- AS102/ttl_records.sh
+++ AS102/ttl_records.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 cd ~/packages/aerospike/aerospike-client-java*/benchmarks
-asinfo -v 'set-config:context=namespace;id=ns1;default-ttl=3h’
+
+asinfo -v "set-config:context=namespace;id=ns1;default-ttl=3h"
 
 ./run_benchmarks -n ns1 -s testset -k 100000 -S 1 -o S:2048 -w I -z 8
 
-asinfo -v 'set-config:context=namespace;id=ns1;default-ttl=4h’
+asinfo -v "set-config:context=namespace;id=ns1;default-ttl=4h"
 
 ./run_benchmarks -n ns1 -s testset -k 100000 -S 100001 -o S:2048 -w I -z 8
 
-asinfo -v 'set-config:context=namespace;id=ns1;default-ttl=5h’
+asinfo -v "set-config:context=namespace;id=ns1;default-ttl=5h"
 
 ./run_benchmarks -n ns1 -s testset -k 100000 -S 200001 -o S:2048 -w I -z 8
