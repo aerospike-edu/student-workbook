ff102c7796231e7ce7f12f8f38cddf35283044b6
pygupta <pgupta@aerospike.com>
2017-10-10 16:12:29 -0700

    script update, pyhton client fixed

--- AS101/as102to101.sh
+++ AS101/as102to101.sh
@@ -17,9 +17,11 @@ sudo cp ~/student-workbook/AS101/orig_aerospike.conf /etc/aerospike/aerospike.co
 #git checkout -b 2.2.2-ami-fix origin/2.2.2-ami-fix
 #git submodule update --init
 #sudo python setup.py install
-sudo rm -rf /tmp/pip-build-root/aerospike
-sudo pip install -vvv -U --index-url https://test.pypi.org/simple/ aerospike
-#sudo pip install aerospike -U
+
+#sudo rm -rf /tmp/pip-build-root/aerospike
+#sudo pip install -vvv -U --index-url https://test.pypi.org/simple/ aerospike
+
+sudo pip install aerospike -U
 #For Stream UDFs in python client, students working on AWS
 sudo mkdir /usr/local/aerospike/usr-lua
 sudo chown aerotraining /usr/local/aerospike/usr-lua
