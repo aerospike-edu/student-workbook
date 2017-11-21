edcf90781cb6221c247e991a4a817390b83663db
pygupta <pgupta@aerospike.com>
2017-11-21 13:38:59 -0800

    XDR DC Test

--- AS202/conn_setup.sh
+++ AS202/conn_setup.sh
@@ -7,6 +7,6 @@ sudo service amc stop
 sudo service aerospike stop
 sudo rm /opt/aerospike/data/ns1.dat
 sudo rm /opt/aerospike/digestlog
-sudo cp ~/student-workbook/AS202/tls_aerospike.conf /etc/aerospike/aerospike.conf
+sudo cp ~/student-workbook/AS202/conn_aerospike.conf /etc/aerospike/aerospike.conf
 sudo service aerospike start
 sudo service amc start
--- AS202/xdr_setup.sh
+++ AS202/xdr_setup.sh
@@ -7,6 +7,7 @@ cd ~
 sudo service amc stop
 sudo service aerospike stop
 sudo rm /opt/aerospike/data/ns1.dat
+sudo rm /opt/aerospike/digestlog
 sudo cp ~/student-workbook/AS202/xdr_aerospike.conf /etc/aerospike/aerospike.conf
 sudo service aerospike start
 sudo service amc start
