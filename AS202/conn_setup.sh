ab15740236e091d625a2edb8fcbe1eeb3e1932b6
pygupta <pgupta@aerospike.com>
2017-11-21 20:18:09 -0800

    dc testing

--- AS202/conn_setup.sh
+++ AS202/conn_setup.sh
@@ -7,6 +7,8 @@ sudo service amc stop
 sudo service aerospike stop
 sudo rm /opt/aerospike/data/ns1.dat
 sudo rm /opt/aerospike/digestlog
+sudo rm /var/log/aerospike/aerospike.log
+sudo rm /var/log/aerospike/xdr.log
 sudo cp ~/student-workbook/AS202/conn_aerospike.conf /etc/aerospike/aerospike.conf
 sudo service aerospike start
 sudo service amc start
