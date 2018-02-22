5b7c3240df899e5ea4ba221f13017dfd351cd124
pygupta <pgupta@aerospike.com>
2018-02-22 14:04:28 -0800

    AS202 Wip

--- AS202/TLS/tls_setup.sh
+++ AS202/TLS/tls_setup.sh
@@ -6,9 +6,9 @@
 sudo service amc stop
 sudo service aerospike stop
 sudo rm /opt/aerospike/data/ns1.dat
-sudo rm /opt/aerospike/digestlog
+sudo rm /opt/aerospike/data/ns2.dat
 sudo rm /var/log/aerospike/aerospike.log
-sudo rm /var/log/aerospike/xdr.log
-sudo cp ~/student-workbook/AS201/tls_aerospike.conf /etc/aerospike/aerospike.conf
+sudo rm /var/log/aerospike/security.log
+sudo cp ~/student-workbook/AS202/TLS/tls_aerospike.conf /etc/aerospike/aerospike.conf
 sudo service aerospike start
 sudo service amc start
