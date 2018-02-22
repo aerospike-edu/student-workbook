effb84a785e886346c019b35e5aeb7f3581206e5
pygupta <pgupta@aerospike.com>
2018-02-22 10:22:43 -0800

    AS202 wip

--- AS202/ca_setup.sh
+++ AS202/ca_setup.sh
@@ -3,11 +3,11 @@
 # Setup for generating TLS certificates for exercises
 # ---
 
-sudo mkdir /CA
-sudo chown aerotraining:aerotraining /CA
-cd /CA
+cd ~/student-workbook/AS201
+mkdir ./CA
+cd ./CA
 echo '01'> serial
 touch index.txt
 mkdir private
 mkdir newcerts
-cp ~/student-workbook/AS202/openssl.cnf ./openssl.cnf
+cp ~/student-workbook/AS201/openssl.cnf ./openssl.cnf
