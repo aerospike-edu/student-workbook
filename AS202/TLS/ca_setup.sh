e9e37c0934173af92c0e52654855b81062961d0d
pygupta <pgupta@aerospike.com>
2018-02-22 13:28:43 -0800

    AS202 wip

--- AS202/TLS/ca_setup.sh
+++ AS202/TLS/ca_setup.sh
@@ -3,11 +3,11 @@
 # Setup for generating TLS certificates for exercises
 # ---
 
-cd ~/student-workbook/AS201
+cd ~/student-workbook/AS202/TLS
 mkdir ./CA
 cd ./CA
 echo '01'> serial
 touch index.txt
 mkdir private
 mkdir newcerts
-cp ~/student-workbook/AS201/openssl.cnf ./openssl.cnf
+cp ~/student-workbook/AS202/openssl.cnf ./openssl.cnf
