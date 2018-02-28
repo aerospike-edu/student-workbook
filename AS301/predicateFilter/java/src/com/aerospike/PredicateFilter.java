ae51e9e4ec39a37c8f6a2d5dc08b54a39b77942d
pygupta <pgupta@aerospike.com>
2018-02-27 22:57:56 -0800

    AS301 wip

--- AS301/predicateFilter/java/src/com/aerospike/PredicateFilter.java
+++ AS301/predicateFilter/java/src/com/aerospike/PredicateFilter.java
@@ -11,6 +11,7 @@ import com.aerospike.client.query.IndexType;
 import com.aerospike.client.query.RecordSet;
 import com.aerospike.client.query.RegexFlag;
 import com.aerospike.client.query.Statement;
+import com.aerospike.client.query.Filter;
 import com.aerospike.client.query.PredExp;
 
 public class PredicateFilter {
@@ -22,22 +23,27 @@ public class PredicateFilter {
 		client = new AerospikeClient(null, "127.0.0.1", 3000);
 		writePolicy = new WritePolicy();
 		queryPolicy = new QueryPolicy();
+	        try  {
+	          client.createIndex(new Policy(), "test", "testset", "country_idx", "country", IndexType.STRING);
+                }
+                catch(Exception e){
+                }
 	}
 
-	public void write(Key key, Bin bin1, Bin bin2) {
-		client.put(writePolicy, key, bin1, bin2);
-		client.createIndex(new Policy(), "test", "Test", "city_idx", "city", IndexType.STRING);
+	public void write(Key key, Bin bin1, Bin bin2, Bin bin3) {
+		client.put(writePolicy, key, bin1, bin2, bin3);
 	}
 
 	public void read () {
 		Record record = null;
 		Statement stmt = new Statement();
-		stmt.setSetName("test");
+		stmt.setSetName("testset");
 		stmt.setNamespace("test");
-		stmt.setIndexName("city_idx");
+		stmt.setIndexName("country_idx");
+		stmt.setFilter(Filter.equal("country","USA"));
         	stmt.setPredExp(
             		PredExp.stringBin("city"),
-            		PredExp.stringValue(".*Diego"),
+            		PredExp.stringValue("U.*Diego"),
             		PredExp.stringRegex(RegexFlag.ICASE | RegexFlag.NEWLINE)
             	);
 
@@ -52,20 +58,23 @@ public class PredicateFilter {
 
 	public static void main(String[] args) {
 		PredicateFilter rc= new PredicateFilter();
-		Key key = new Key("test","test",123);
+		Key key = new Key("test","testset",123);
 		Bin bin1 = new Bin("city", "US-SanDiego");
 		Bin bin2 = new Bin("empid", "123456");
-		rc.write(key, bin1, bin2);
+		Bin bin3 = new Bin("country", "USA");
+		rc.write(key, bin1, bin2, bin3);
 
-		key = new Key("test","test",124);
+		key = new Key("test","testset",124);
 		bin1 = new Bin("city", "Peru-SanDiego");
 		bin2 = new Bin("empid", "784232");
-		rc.write(key, bin1, bin2);
+		bin3 = new Bin("country", "Peru");
+		rc.write(key, bin1, bin2, bin3);
 
-		key = new Key("test","test",125);
+		key = new Key("test","testset",125);
 		bin1 = new Bin("city", "US-LosAngeles");
 		bin2 = new Bin("empid", "454231");
-		rc.write(key, bin1, bin2);
+		bin3 = new Bin("country", "USA");
+		rc.write(key, bin1, bin2, bin3);
 
 		rc.read();
 	}
