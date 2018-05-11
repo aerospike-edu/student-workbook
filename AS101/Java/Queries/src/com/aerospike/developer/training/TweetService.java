6ccb1af30eac1ead5161ebd37714d7440c659153
pygupta <pgupta@aerospike.com>
2018-05-11 13:46:01 -0700

    AS101 update, see StudentWorkbookChangeLog.txt

--- AS101/Java/Queries/src/com/aerospike/developer/training/TweetService.java
+++ AS101/Java/Queries/src/com/aerospike/developer/training/TweetService.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * Copyright 2012-2015 Aerospike, Inc.
  *
  * Portions may be licensed to Aerospike, Inc. under one or more contributor
@@ -52,17 +52,17 @@ public class TweetService {
 	public void createTweet() throws AerospikeException, InterruptedException {
 
 		console.printf("\n********** Create Tweet **********\n");
-		
+
 	    ///*********************///
 	    ///*****Data Model*****///
 	    //Namespace: test
 	    //Set: tweets
 	        //Key: <username:<counter>>
 	        //Bins:
-	            //tweet - string 
+	            //tweet - string
 	            //ts - int (Stores epoch timestamp of the tweet)
 	            //username - string
-	
+
 		    //Sample Key: dash:1
 		    //Sample Record:
 		        //{ tweet: 'Put. A. Bird. On. It.',
@@ -82,7 +82,7 @@ public class TweetService {
 
 		if (username != null && username.length() > 0) {
 			// TODO: Check if userRecord exists
-			// Exercise K2			
+			// Exercise K2
 			userKey = new Key("test", "users", username);
 			userRecord = client.get(null, userKey);
 			if (userRecord != null) {
@@ -97,13 +97,13 @@ public class TweetService {
 				// Create timestamp to store along with the tweet so we can
 				// query, index and report on it
 				long ts = getTimeStamp();
-				
+
 				// TODO: Create WritePolicy instance
 			    // Exercise K2
 				WritePolicy wPolicy = new WritePolicy();
 				wPolicy.recordExistsAction = RecordExistsAction.UPDATE;
-				
-				// TODO: Create Key and Bin instances for the tweet record. 
+
+				// TODO: Create Key and Bin instances for the tweet record.
 				// HINT: tweet key should be in username:nextTweetCount format
 			    // Exercise K2
 				tweetKey = new Key("test", "tweets", username + ":"
@@ -111,7 +111,7 @@ public class TweetService {
 				Bin bin1 = new Bin("tweet", tweet);
 				Bin bin2 = new Bin("ts", ts);
 				Bin bin3 = new Bin("username", username);
-				
+
 				// TODO: Write tweet record
 			    // Exercise K2
 
@@ -120,24 +120,24 @@ public class TweetService {
 
 				// TODO: Update tweet count and last tweeted timestamp in the user
 				// record. Add code in updateUser()
-	            // Exercise K2  
+	            // Exercise K2
 				updateUser(client, userKey, wPolicy, ts, nextTweetCount);
 			} else {
 				console.printf("ERROR: User record not found!\n");
 			}
 		}
 	} //createTweet
-		
+
 	public void scanAllTweetsForAllUsers() {
 		try {
 			// Java Scan
-			// TODO: Create ScanPolicy instance 
+			// TODO: Create ScanPolicy instance
 			// Exercise K4
 			ScanPolicy policy = new ScanPolicy();
 			policy.concurrentNodes = true;
 			policy.priority = Priority.LOW;
 			policy.includeBinData = true;
-			
+
 			// TODO: Initiate scan operation that invokes callback for outputting tweets on the console
 			// Exercise K4
 			client.scanAll(policy, "test", "tweets", new ScanCallback() {
@@ -154,16 +154,16 @@ public class TweetService {
 					+ UtilityService.printStackTrace(e));
 		}
 	} //scanAllTweetsForAllUsers
-	
+
 	private void updateUser(AerospikeClient client, Key userKey,
 			WritePolicy policy, long ts, int tweetCount) throws AerospikeException, InterruptedException {
-		
+
 		// TODO: Update tweet count and last tweeted timestamp in the user record
 	    // Exercise K2
         client.put(policy, userKey, new Bin("tweetcount", tweetCount), new Bin("lasttweeted", ts));
         console.printf("\nINFO: The tweet count now is: " + tweetCount);
 
-        
+
         //Exercise K6
         //Comment above code and uncomment call to updateUserUsingOperate()
         //Add code in updateUserUsingOperate()
@@ -172,34 +172,39 @@ public class TweetService {
 
 	private void updateUserUsingOperate(AerospikeClient client, Key userKey,
 			WritePolicy policy, long ts) throws AerospikeException {
-		
-		// TODO: Initiate operate passing in policy, user record key, 
-		// .add operation incrementing tweet count, .put operation updating timestamp 
-		// and .get operation to read the user record		
+
+		// TODO: Initiate operate passing in policy, user record key,
+		// .add operation incrementing tweet count, .put operation updating timestamp
+		// and .get operation to read the user record
 	    // Exercise K6
-		
+
 		Record record = client.operate(policy, userKey,
 				Operation.add(new Bin("tweetcount", 1)),
 				Operation.put(new Bin("lasttweeted", ts)),
 				Operation.get());
-		
-		// TODO: Output most recent tweet count     
+
+		// TODO: Output most recent tweet count
 	    // Exercise K6
 		console.printf("\nINFO: The tweet count now is: " + record.getValue("tweetcount"));
 	} //updateUserUsingOperate
 
-	public void queryTweetsByUsername() throws AerospikeException {		
+	public void queryTweetsByUsername() throws AerospikeException {
 		console.printf("\n********** Query Tweets By Username **********\n");
-		
+
 		//TODO: Create STRING index on username in tweets set
 		//Exercise Q3
-		// NOTE: Index creation has been included in here for convenience and to demonstrate the syntax. 
+		// NOTE: Index creation has been included in here for convenience and to demonstrate the syntax.
 	    // The recommended way of creating indexes in production env is via AQL
 		// or create once using standalone application code.
-		
+
 		console.printf("TODO: Create STRING index on username in tweets set.\n");
-		//IndexTask task = .....
-		//task.waitTillComplete(100);
+    // Do Index creation within try-catch. If index already exists, indexCreate() will error out.
+		//try {
+    //  IndexTask task = .....
+		//  task.waitTillComplete(100);
+    // } catch (Exception e) {
+    // System.out.printf(e.toString());
+    // }
 
 		RecordSet rs = null;
 		try {
@@ -209,42 +214,42 @@ public class TweetService {
 			username = console.readLine();
 
 			if (username != null && username.length() > 0) {
-				// TODO: Create String array of bins you would like to retrieve. 
+				// TODO: Create String array of bins you would like to retrieve.
 				// In this example, we want to display all tweets for a given user.
 				// Exercise Q3
 				console.printf("TODO: Create String array of bins you would like to retrieve.\n");
 				//String[] bins = .....
-				
+
 				// TODO: Create Statement instance
 				// Exercise Q3
-				console.printf("TODO: Create Statement instance.\n");				
-				
+				console.printf("TODO: Create Statement instance.\n");
+
 				// TODO: Set namespace on the instance of Statement
 				// Exercise Q3
 				console.printf("TODO: Set namespace on the instance of Statement.\n");
-				
+
 				// TODO: Set name of the set on the instance of Statement
 				// Exercise Q3
 				console.printf("TODO: Set name of the set on the instance of Statement.\n");
-				
+
 				// TODO: Set name of the index on the instance of Statement
 			    // Exercise Q3
 				console.printf("TODO: Set name of the index on the instance of Statement.\n");
-				
-				
+
+
 				// TODO: Set list of bins you want retrieved on the instance of Statement
 				// Exercise Q3
 				console.printf("TODO: Set list of bins you want retrieved on the instance of Statement.\n");
-				
+
 				// TODO: Set equality Filter on username on the instance of Statement
-			    // Exercise Q3				
+			    // Exercise Q3
 				console.printf("TODO: Set equality Filter on username on the instance of Statement.\n");
-				
+
 				// TODO: Execute query passing in <null> policy and instance of Statement
 				// Exercise Q3
 				console.printf("TODO: Execute query passing in <null> policy and instance of Statement.\n");
-				
-				
+
+
 				console.printf("\nHere's " + username + "'s tweet(s):\n");
 				console.printf("TODO: Iterate through returned RecordSet and output tweets to the console.\n");
 				while (rs.next()) {
@@ -265,18 +270,23 @@ public class TweetService {
 
 	public void queryUsersByTweetCount() throws AerospikeException {
 		console.printf("\n********** Query Users By Tweet Count Range **********\n");
-		
+
 		//TODO: Create NUMERIC index on tweetcount in users set
 		//Exercise Q4
-		// NOTE: Index creation has been included in here for convenience and to demonstrate the syntax. 
+		// NOTE: Index creation has been included in here for convenience and to demonstrate the syntax.
 		// The recommended way of creating indexes in production env is via AQL
 		// or create once using standalone application code.
-		
+
 		console.printf("TODO: Create NUMERIC index on tweetcount in users set.\n");
-		
-		//IndexTask task = ....
-		//task.waitTillComplete(100);
-		
+
+    // Do Index creation within try-catch. If index already exists, indexCreate() will error out.
+		//try {
+    //  IndexTask task = .....
+		//  task.waitTillComplete(100);
+    // } catch (Exception e) {
+    // System.out.printf(e.toString());
+    // }
+
 		RecordSet rs = null;
 		try {
 			// Get min and max tweet counts
@@ -287,64 +297,64 @@ public class TweetService {
 			console.printf("Enter Max Tweet Count:");
 			max = Integer.parseInt(console.readLine());
 
-            // TODO: Create String array of bins you would like to retrieve. 
-			// In this example, we want to output which user has how many tweets. 
+            // TODO: Create String array of bins you would like to retrieve.
+			// In this example, we want to output which user has how many tweets.
             // Exercise Q4
 			console.printf("TODO: Create String array of bins you would like to retrieve..\n");
 			//String[] bins = .....
-			
+
 			// TODO: Create Statement instance
 			// Exercise Q4
 			console.printf("TODO: Create Statement instance.\n");
-			
-			
+
+
 			// TODO: Set namespace on the instance of Statement
 		    // Exercise Q4
 			console.printf("TODO: Set namespace on the instance of Statement.\n");
-			
-			
+
+
 			// TODO: Set name of the set on the instance of Statement
 		    // Exercise Q4
 			console.printf("TODO: Set name of the set on the instance of Statement.\n");
-			
-			
+
+
 			// TODO: Set name of the index on the instance of Statement
 		    // Exercise Q4
 			console.printf("TODO: Set name of the index on the instance of Statement.\n");
-			
-			
+
+
 			// TODO: Set list of bins you want retrieved on the instance of Statement
 		    // Exercise Q4
 			console.printf("TODO: Set list of bins you want retrieved on the instance of Statement.\n");
-			
-			
+
+
 			// TODO: Set min--max range Filter on tweetcount on the instance of Statement
 		    // Exercise Q4
 			console.printf("TODO: Set min--max range Filter on tweetcount on the instance of Statement.\n");
-			
+
 
 			// TODO: Execute query passing in <null> policy and instance of Statement
 		    // Exercise Q4
 			console.printf("TODO: Execute query passing in <null> policy and instance of Statement.\n");
-			
-			
+
+
 			console.printf("\nList of users with " + min + "-" + max
 					+ " tweets:\n");
-			
+
 			console.printf("TODO: Iterate through returned RecordSet and for each record, output text in format <username> has <#> tweets.\n");
 			while (rs.next()) {
 				Record r = rs.getRecord();
 				// TODO: Iterate through returned RecordSet and for each record, output text in format "<username> has <#> tweets"
-			    // Exercise Q4				
+			    // Exercise Q4
 				//console.printf(....
 				}
 			} finally {
-				// TODO: Close record set 
+				// TODO: Close record set
 				// Exercise Q4
-				console.printf("TODO: Close record set.\n");				
+				console.printf("TODO: Close record set.\n");
 		}
 	} //queryUsersByTweetCount
-	
+
 	public void createTweets() throws AerospikeException {
 		String[] randomTweets = {
 				"For just $1 you get a half price download of half of the song and listen to it just once.",
@@ -377,7 +387,7 @@ public class TweetService {
 		int maxTweets = 20;
 		String username;
 		long ts = 0;
-		
+
 		WritePolicy wPolicy = new WritePolicy();
 		wPolicy.recordExistsAction = RecordExistsAction.UPDATE;
 
