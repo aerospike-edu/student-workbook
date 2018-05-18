042f3fefc181aa774d17b6d07e7854a01fc85728
Piyush Gupta <pgupta@aerospike.com>
2018-05-17 22:52:50 -0700

    smallRecords

--- AS301/smallRecords/java/src/com/aerospike/SmallRecords.java
+++ AS301/smallRecords/java/src/com/aerospike/SmallRecords.java
@@ -14,14 +14,14 @@ import com.aerospike.client.policy.WritePolicy;
 public class SmallRecords {
 	public static void largeRecord(AerospikeClient client, String keyString) {
 		Key key = new Key("test", "testList", "id1234");
-		client.operate(null, key, ListOperation.insert("myList",0, Value.get(value)),
+		client.operate(null, key, ListOperation.insert("myList",0, Value.get(keyString)),
 				ListOperation.trim("myList", 0, 20));
 		
 
 	}
 	public static void main(String[] args) {
 		AerospikeClient client = new AerospikeClient("127.0.0.1", 3000);
-	        maxRecords = 25;	
+	    int  maxRecords = 25;	
 		for (int i = 0; i < maxRecords; i++) {
 		  Key key = new Key("test", "smallRecordsSet", "id"+i);
 		  client.put(null, key, new Bin("id", "id"+i), new Bin("name", "name"+i), new Bin ("ver", i));
