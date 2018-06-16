a034147d0f77f71b2b9f8312cfaac5edda365930
pygupta <pgupta@aerospike.com>
2018-06-15 19:54:34 -0700

    AS301 changes

---
+++ AS301/setupCappedMap.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# ------------------------------
+# Setup test namespace for CappedMap
+# ---
+
+cd ~
+sudo service aerospike stop
+sudo service amc stop
+sudo rm /opt/aerospike/data/test.dat
+sudo rm /var/log/aerospike/aerospike.log
+sudo cp ~/student-workbook/AS301/cappedMap_aerospike.conf /etc/aerospike/aerospike.conf
+sudo service aerospike start
+sudo service amc start
