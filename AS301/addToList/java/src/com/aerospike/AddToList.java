71a56dacd0dc1eb526cfe94ac618d5d120d7aaed
Piyush Gupta <pgupta@aerospike.com>
2018-07-02 20:32:33 -0700

    AS301 AddToList

--- AS301/addToList/java/src/com/aerospike/AddToList.java
+++ AS301/addToList/java/src/com/aerospike/AddToList.java
@@ -41,6 +41,20 @@ public class AddToList {
                          );
                 }
 	}
+	public static void insertUnordered_setOrder(AerospikeClient client, int value) {
+		Key key = new Key("test", "s1", 3);  //Record 3 
+                ListPolicy lPolicy = new ListPolicy(ListOrder.UNORDERED, ListWriteFlags.DEFAULT); 
+                if(value != 0){  //First loop items, idenitfy List Order. UNORDERED is also DEFAULT 
+		client.operate(null, key, 
+                          ListOperation.append(lPolicy, "myList", Value.get(value))
+                        );
+                } else {
+		client.operate(null, key,   //Comment out setOrder and retry. 
+                          ListOperation.setOrder("myList", ListOrder.ORDERED), 
+                          ListOperation.append("myList", Value.get(value))
+                         );
+                }
+	}
 
 
 	public static void main(String[] args) {
@@ -49,26 +63,32 @@ public class AddToList {
 
 		Key key1 = new Key("test", "s1", 1);
 		Key key2 = new Key("test", "s1", 2);
+		Key key3 = new Key("test", "s1", 3);
 		WritePolicy policy = new WritePolicy();
 		policy.recordExistsAction = RecordExistsAction.UPDATE;
 		
 		client.delete(policy, key1);
 		client.delete(policy, key2);
+		client.delete(policy, key3);
 
                 Bin bin1 = new Bin("id","key1");
                 Bin bin2 = new Bin("id","key2");
+                Bin bin3 = new Bin("id","key3");
 
                 client.put(policy, key1, bin1);
                 client.put(policy, key2, bin2);
+                client.put(policy, key3, bin3);
 
 
 		for (int i = 10; i < 20; i++) {
 			insertOrdered(client,  i);
 			insertUnordered(client,  i);
+			insertUnordered_setOrder(client,  i);
 		}
 		for (int i = 0; i < 10; i++) {
 			insertOrdered(client,  i);
 			insertUnordered(client,  i);
+			insertUnordered_setOrder(client,  i);
 		}
 		client.close();
 	}
