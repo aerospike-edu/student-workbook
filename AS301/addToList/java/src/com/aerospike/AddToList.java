181c77e74f00329cefa1f44f7139ab0f4a3658b5
Piyush Gupta <pgupta@aerospike.com>
2018-07-03 12:07:14 -0700

    AS301 AddToList Updated

--- AS301/addToList/java/src/com/aerospike/AddToList.java
+++ AS301/addToList/java/src/com/aerospike/AddToList.java
@@ -15,20 +15,20 @@ import com.aerospike.client.policy.RecordExistsAction;
 import com.aerospike.client.policy.WritePolicy;
 
 public class AddToList {
-	public static void insertOrdered(AerospikeClient client, int value) {
+	public static void appendOrdered(AerospikeClient client, int value) {
 		Key key = new Key("test", "s1", 1);  //Record 1
                 ListPolicy lPolicy = new ListPolicy(ListOrder.ORDERED, ListWriteFlags.DEFAULT); 
                 if(value == 10){  //First item, idenitfy List Order. Extra Credit A) Try changing 10 to 11!
 		client.operate(null, key, 
                           ListOperation.append(lPolicy, "myList", Value.get(value))
                         );
-                } else {
+                } else  {
 		client.operate(null, key, 
                           ListOperation.append("myList", Value.get(value))
                          );
-                }
+                } 
 	}
-	public static void insertUnordered(AerospikeClient client, int value) {
+	public static void appendUnordered(AerospikeClient client, int value) {
 		Key key = new Key("test", "s1", 2);  //Record 2
                 ListPolicy lPolicy = new ListPolicy(ListOrder.UNORDERED, ListWriteFlags.DEFAULT); 
                 if(value == 10){  //First item, idenitfy List Order. UNORDERED is also DEFAULT 
@@ -41,7 +41,7 @@ public class AddToList {
                          );
                 }
 	}
-	public static void insertUnordered_setOrder(AerospikeClient client, int value) {
+	public static void appendUnordered_setOrder(AerospikeClient client, int value) {
 		Key key = new Key("test", "s1", 3);  //Record 3 
                 ListPolicy lPolicy = new ListPolicy(ListOrder.UNORDERED, ListWriteFlags.DEFAULT); 
                 if(value != 0){  //First loop items, idenitfy List Order. UNORDERED is also DEFAULT 
@@ -55,6 +55,24 @@ public class AddToList {
                          );
                 }
 	}
+	public static void appendOrdered_insertUnordered(AerospikeClient client, int value) {
+		Key key = new Key("test", "s1", 4);  //Record 1
+                ListPolicy lPolicy = new ListPolicy(ListOrder.ORDERED, ListWriteFlags.DEFAULT); 
+                if(value == 10){  //First item, idenitfy List Order. Extra Credit A) Try changing 10 to 11!
+		client.operate(null, key, 
+                          ListOperation.append(lPolicy, "myList", Value.get(value))
+                        );
+                } else if(value > 10)  {
+		client.operate(null, key, 
+                          ListOperation.append("myList", Value.get(value))
+                         );
+                } else {
+		client.operate(null, key, 
+                          ListOperation.setOrder("myList", ListOrder.UNORDERED),
+                          ListOperation.insert("myList", -2, Value.get(value))
+                         );
+                }
+	}
 
 
 	public static void main(String[] args) {
@@ -64,31 +82,32 @@ public class AddToList {
 		Key key1 = new Key("test", "s1", 1);
 		Key key2 = new Key("test", "s1", 2);
 		Key key3 = new Key("test", "s1", 3);
+		Key key4 = new Key("test", "s1", 4);
 		WritePolicy policy = new WritePolicy();
 		policy.recordExistsAction = RecordExistsAction.UPDATE;
 		
 		client.delete(policy, key1);
 		client.delete(policy, key2);
 		client.delete(policy, key3);
+		client.delete(policy, key4);
 
-                Bin bin1 = new Bin("id","key1");
-                Bin bin2 = new Bin("id","key2");
-                Bin bin3 = new Bin("id","key3");
-
-                client.put(policy, key1, bin1);
-                client.put(policy, key2, bin2);
-                client.put(policy, key3, bin3);
+                client.put(policy, key1, new Bin("id","key1"));
+                client.put(policy, key2, new Bin("id","key2"));
+                client.put(policy, key3, new Bin("id","key3"));
+                client.put(policy, key4, new Bin("id","key4"));
 
 
 		for (int i = 10; i < 20; i++) {
-			insertOrdered(client,  i);
-			insertUnordered(client,  i);
-			insertUnordered_setOrder(client,  i);
+			appendOrdered(client,  i);
+			appendUnordered(client,  i);
+			appendUnordered_setOrder(client,  i);
+			appendOrdered_insertUnordered(client,  i);
 		}
 		for (int i = 0; i < 10; i++) {
-			insertOrdered(client,  i);
-			insertUnordered(client,  i);
-			insertUnordered_setOrder(client,  i);
+			appendOrdered(client,  i);
+			appendUnordered(client,  i);
+			appendUnordered_setOrder(client,  i);
+			appendOrdered_insertUnordered(client,  i);
 		}
 		client.close();
 	}
