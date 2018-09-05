2dd7d64ecba3335b356bf914c19ab4991f15a3a5
pygupta <aerotraining@ip-172-31-56-168.ec2.internal>
2018-09-05 22:58:17 +0000

    AS103

--- AS103/sc_setup.sh
+++ AS103/sc_setup.sh
@@ -8,6 +8,6 @@ sudo service aerospike stop
 sudo service amc stop
 sudo rm /opt/aerospike/data/ns1f1.dat
 sudo rm /opt/aerospike/data/ns1f2.dat
-sudo cp ~/student-workbook/AS102/sc_aerospike.conf /etc/aerospike/aerospike.conf
+sudo cp ~/student-workbook/AS103/sc_aerospike.conf /etc/aerospike/aerospike.conf
 sudo service aerospike start
 sudo service amc start
