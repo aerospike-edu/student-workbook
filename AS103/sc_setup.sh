95226c2d67f0e27c2f6b41f2e532cfd8293af6bb
pygupta <aerotraining@ip-172-31-56-168.ec2.internal>
2018-09-05 22:38:01 +0000

    AS103 Material Added

---
+++ AS103/sc_setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# ------------------------------
+# Setup for Strong Consistency Mode exercises
+# ---
+
+cd ~
+sudo service aerospike stop
+sudo service amc stop
+sudo rm /opt/aerospike/data/ns1f1.dat
+sudo rm /opt/aerospike/data/ns1f2.dat
+sudo cp ~/student-workbook/AS102/sc_aerospike.conf /etc/aerospike/aerospike.conf
+sudo service aerospike start
+sudo service amc start
