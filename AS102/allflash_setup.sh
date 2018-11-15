452de3af3436c9156a4aee087e3ad19ee8a7ea0f
pygupta <pgupta@aerospike.com>
2018-11-15 10:01:14 -0800

    allflash update

--- AS102/allflash_setup.sh
+++ AS102/allflash_setup.sh
@@ -5,9 +5,11 @@
 
 cd ~
 sudo service aerospike stop
+sudo service amc stop
 sudo rm /opt/aerospike/data/ns1.dat
 sudo rm -r /opt/aerospike/data/ns1pi
 sudo mkdir /opt/aerospike/data/ns1pi
 sudo chown aerospike /opt/aerospike/data/ns1pi
-sudo cp ~/student-workbook/AS102/quiesce_aerospike.conf /etc/aerospike/aerospike.conf
+sudo cp ~/student-workbook/AS102/allflash_aerospike.conf /etc/aerospike/aerospike.conf
 sudo service aerospike start
+sudo service amc start
