6df7832d80f21d5d6e64ec7909675d8b45c1bb5e
pygupta <pgupta@aerospike.com>
2019-02-12 14:37:21 -0800

    af update

--- af/allflash_setup.sh
+++ af/allflash_setup.sh
@@ -6,10 +6,11 @@
 cd ~
 sudo service aerospike stop
 sudo service amc stop
-sudo rm /opt/aerospike/data/ns1.dat
-sudo rm -r /opt/aerospike/data/ns1pi
-sudo mkdir /opt/aerospike/data/ns1pi
-sudo chown aerospike /opt/aerospike/data/ns1pi
-sudo cp ~/student-workbook/AS102/allflash_aerospike.conf /etc/aerospike/aerospike.conf
+sudo rm /opt/aerospike/data/allflash.dat
+sudo rm /opt/aerospike/data/in_mem.dat
+sudo rm -r /opt/aerospike/data/pi
+sudo mkdir /opt/aerospike/data/pi
+sudo chown aerospike /opt/aerospike/data/pi
+sudo cp ~/student-workbook/af/allflash_aerospike.conf /etc/aerospike/aerospike.conf
 sudo service aerospike start
 sudo service amc start
