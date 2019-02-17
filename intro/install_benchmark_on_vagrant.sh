6b8b5b653727bd1842cb4b9adb6db22628f96993
pygupta <pgupta@aerospike.com>
2019-02-17 11:18:57 -0800

    intro update

--- intro/install_benchmark_on_vagrant.sh
+++ intro/install_benchmark_on_vagrant.sh
@@ -3,6 +3,14 @@
 # Install Java Benchmark
 # -
 cd ~
+sudo yum install -y java-1.8.0-openjdk-devel
+java -version
+cd /opt
+sudo wget http://www-eu.apache.org/dist/maven/maven-3/3.5.4/binaries/apache-maven-3.5.4-bin.tar.gz
+sudo tar xzf apache-maven-3.5.4-bin.tar.gz
+sudo ln -s apache-maven-3.5.4 maven
+sudo cp ~/student-workbook/intro/maven.sh /etc/profile.d/maven.sh
+source /etc/profile/maven.sh
 wget -O aerospike-client-java.tgz  http://www.aerospike.com/download/client/java/latest/artifact/tgz
 cd ~/packages/aerospike
 tar xvf aerospike-client-java.tgz
