07601066a46f43a7035f402595ee5a789da1f477
pygupta <pgupta@aerospike.com>
2019-02-21 18:06:56 -0800

    Update scripts

--- intro/setup.sh
+++ intro/setup.sh
@@ -4,8 +4,13 @@
 # ---
 
 cd ~
-sudo service aerospike stop
 sudo service amc stop
+sudo service aerospike stop
+
+#clean any secondary indexes or data files
+sudo rm /opt/aerospike/data/*.dat
+sudo rm /opt/aerospike/smd/sindex.smd
+
 sudo cp ~/student-workbook/intro/start_aerospike.conf /etc/aerospike/aerospike.conf
 sudo service aerospike start
 sudo service amc start
--- ops/storage_setup.sh
+++ ops/storage_setup.sh
@@ -4,7 +4,13 @@
 # ---
 
 cd ~
+sudo service amc stop
 sudo service aerospike stop
-sudo rm /opt/aerospike/data/ns1.dat
+
+# Clean any secondary indexes and data files
+sudo rm /opt/aerospike/data/*.dat
+sudo rm /opt/aerospike/smd/sindex.smd
+
 sudo cp ~/student-workbook/ops/storage_aerospike.conf /etc/aerospike/aerospike.conf
 sudo service aerospike start
+sudo service amc start
