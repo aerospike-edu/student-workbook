b5569ffb35cb73149a6f6a9d8dc57a710d6354a1
pygupta <pgupta@aerospike.com>
2019-03-03 15:40:01 -0800

    Added sc subdir for SC Mode Course

---
+++ sc/insert100Krecords.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# ------------------------------
+# Load test data using benchmark
+# -
+cd ~/packages/aerospike/aerospike-client-java-*/benchmarks
+./run_benchmarks -h 127.0.0.1 -p 3000 -n ns1 -s s1 -k 100000 -latency "7,1" -S 1 -o S:100 -w I -z 8
