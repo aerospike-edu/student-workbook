3302866b0da9b270917b79588b3df88be2f287ad
pygupta <pgupta@aerospike.com>
2019-06-07 16:26:59 -0700

    ops2 update

--- ops2/nocache.sh
+++ ops2/nocache.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 cd ~/packages/aerospike/aerospike-client-java*/benchmarks
-./run_benchmarks -h 52.66.212.176  -n ns1 -s testset -k 1000 -S 1 -o S:30000 -w I  -z 8
+# Insert 10K records
+./run_benchmarks -h 52.66.212.176  -n ns1 -s testset -k 10000 -S 1 -o S:30000 -w I  -z 8
+
+# Read - Update 10K records.
+./run_benchmarks -h 52.66.212.176  -n ns1 -s testset -k 10000 -S 1 -o S:30000 -w RU,50  -z 8
