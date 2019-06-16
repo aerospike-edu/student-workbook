7c92b3daf5c091aba676557aaec7753138b11001
pygupta <pgupta@aerospike.com>
2019-06-15 22:16:17 -0700

    ops2 udpated

--- ops2/monitor_insert.sh
+++ ops2/monitor_insert.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd ~/packages/aerospike/aerospike-client-java*/benchmarks
-./run_benchmarks -h 127.0.0.1  -n ns1 -s testset -k 10000 -S 1 -o S:16 -w I  -z 1 -g 50
+./run_benchmarks -h 127.0.0.1  -n ns1 -s testset -k 10000 -S 1 -o S:16 -w I
