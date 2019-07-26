1f342ddefd9db2ffaccb830d791a5c1f34940a3e
pygupta <gupta4453@gmail.com>
2019-07-26 14:43:57 -0700

    DataModeling update

--- dm/cacheExample/setupCache.sh
+++ dm/cacheExample/setupCache.sh
@@ -8,6 +8,6 @@ sudo service aerospike stop
 sudo service amc stop
 sudo rm /opt/aerospike/data/Cache.dat
 sudo rm /var/log/aerospike/aerospike.log
-sudo cp ~/student-workbook/AS301/cacheExample/cache_aerospike.conf /etc/aerospike/aerospike.conf
+sudo cp ~/student-workbook/dm/cacheExample/cache_aerospike.conf /etc/aerospike/aerospike.conf
 sudo service aerospike start
 sudo service amc start
