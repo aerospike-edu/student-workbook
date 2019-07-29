736fef8197662bb9069b2bada21dada760e88e49
pygupta <pgupta@aerospike.com>
2019-07-28 19:42:30 -0700

    xdr added

---
+++ xdr/conn_setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# ------------------------------
+# Setup for generating TLS certificates for exercises
+# ---
+
+sudo service amc stop
+sudo service aerospike stop
+sudo rm /opt/aerospike/data/ns1.dat
+sudo rm /opt/aerospike/digestlog
+sudo rm /var/log/aerospike/aerospike.log
+sudo rm /var/log/aerospike/xdr.log
+sudo cp ~/student-workbook/xdr/conn_aerospike.conf /etc/aerospike/aerospike.conf
+sudo service aerospike start
+sudo service amc start
