0e980290ad59023a0578f6319fae8e2c0cf8ac38
pygupta <pgupta@aerospike.com>
2019-10-04 09:40:08 -0700

    Add -y flag to sudo yum

--- dev/setupCClientOnAWS.sh
+++ dev/setupCClientOnAWS.sh
@@ -6,8 +6,8 @@
 
 cd ~/packages/aerospike
 wget -O aerospike-client-c.tgz http://www.aerospike.com/download/client/c/4.1.6/artifact/el6
-sudo yum install openssl-devel
-sudo yum install gcc gcc-c++
+sudo yum -y install openssl-devel
+sudo yum -y install gcc gcc-c++
 tar xvf aerospike-client-c.tgz
 cd aerospike-client-c-4.1.6.el6.x86_64/
 sudo rpm -i aerospike-client-c-devel-4.1.6-1.el6.x86_64.rpm
