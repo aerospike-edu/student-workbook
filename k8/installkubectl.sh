14c1953ce9e0927824a00d942796f95956a09c23
pygupta <pgupta@aerospike.com>
2021-06-29 22:11:42 -0700

    k8 script fix

--- k8/installkubectl.sh
+++ k8/installkubectl.sh
@@ -5,5 +5,5 @@ curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.18.9/2020-11-02/
 chmod +x ./kubectl
 mkdir -p $HOME/bin 
 cp ./kubectl $HOME/bin/kubectl
-$ echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
+echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
 kubectl version --short --client
