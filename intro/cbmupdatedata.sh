810fd9d3ffb99d1927aeb39a0ae61b50cba54e2b
pygupta <pgupta@aerospike.com>
2021-11-23 23:16:47 -0800

    See StudentWorkbookChangelog

---
+++ intro/cbmupdatedata.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# ------------------------------
+# Throw a 90% READ, 10% UPADTE load using C benchmark tool
+# -
+asbench -h 127.0.0.1 -p 3000 -n test -s testset -k 100000 -latency "7,1" -S 1 -o S100 -w RU,90 -z 8
