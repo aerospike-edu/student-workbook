1224f68b41ad3aa964f1641d1f6fd55649e0bd53
pygupta <pgupta@aerospike.com>
2021-11-23 23:41:28 -0800

    See StudentWorkbookChangelog

--- intro/cbminsertdata.sh
+++ intro/cbminsertdata.sh
@@ -2,4 +2,4 @@
 # ------------------------------
 # Load test data using C benchmark
 # -
-asbench -h 127.0.0.1 -p 3000 -n test -s testset -k 100000 -latency "7,1" -S 1 -o S100 -w I -z 8
+asbench -h 127.0.0.1 -p 3000 -n test -s testset -k 100000 -L -S 1 -o S100 -w I -z 8
--- intro/cbmupdatedata.sh
+++ intro/cbmupdatedata.sh
@@ -2,4 +2,4 @@
 # ------------------------------
 # Throw a 90% READ, 10% UPADTE load using C benchmark tool
 # -
-asbench -h 127.0.0.1 -p 3000 -n test -s testset -k 100000 -latency "7,1" -S 1 -o S100 -w RU,90 -t 30 -z 8
+asbench -h 127.0.0.1 -p 3000 -n test -s testset -k 100000 -L -S 1 -o S100 -w RU,90 -t 30 -z 8
