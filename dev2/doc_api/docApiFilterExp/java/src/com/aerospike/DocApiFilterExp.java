817e7bbe8ef5ac4288512a658059833c5e32a01c
pygupta <pgupta@aerospike.com>
2022-01-31 10:33:48 -0800

    DocApi Filter Exp added

--- dev2/doc_api/docApiFilterExp/java/src/com/aerospike/DocApiFilterExp.java
+++ dev2/doc_api/docApiFilterExp/java/src/com/aerospike/DocApiFilterExp.java
@@ -11,6 +11,8 @@ import com.aerospike.client.policy.Policy;
 import com.aerospike.client.cdt.CTX;
 import com.aerospike.client.cdt.ListReturnType;
 
+import com.aerospike.client.policy.WritePolicy;
+
 public class DocApiFilterExp {
     public static void main(String[] args) throws Exception {
       DocApiFilterExp ce = new DocApiFilterExp();
@@ -41,8 +43,6 @@ public class DocApiFilterExp {
 
         System.out.println( docClient.get(doc1,"docBin","$") ); //Print inserted object
 
-        //What was that ^ ?? Explained below:
-
         System.out.println("\nRead list item at index 1 for map value at key=k3");
         //Read the document specified by jsonpath in a given bin
         String jsonPath = "$.k3[1]" ; //Expect "v32"
@@ -61,18 +61,121 @@ public class DocApiFilterExp {
                     Exp.val(0), 
                     Exp.mapBin("docBin"), 
                     CTX.mapKey(Value.get("k2")) ),
-                Exp.val(1) 
+                Exp.val(2) 
+              )
+            );
+        rPolicy.failOnFilteredOut = false;  //Default is false 
+
+        obj = docClient.get(rPolicy, doc1, "docBin", jsonPath);  //Using Read Filter Expression 
+        if(obj != null) {
+          System.out.println("Document Client Read Filter Expression [k2[0]>2] (false): "+obj.toString());
+        } else {
+          System.out.println("Document Client Read Filter Expression [k2[0]>2] (false) returned null object");
+        }
+       
+        Record r = client.get(rPolicy, doc1);  //Using Read Filter Expression
+        System.out.println("AerospikeClient Read Filter Expression [k2[0]>2 (false): "+ r);
+
+        //Read filter true
+        rPolicy.filterExp = Exp.build( 
+              Exp.gt( 
+                ListExp.getByIndex(
+                    ListReturnType.VALUE, 
+                    Exp.Type.INT, 
+                    Exp.val(0), 
+                    Exp.mapBin("docBin"), 
+                    CTX.mapKey(Value.get("k2")) ),
+                Exp.val(0) 
               )
             );
         rPolicy.failOnFilteredOut = false;
         obj = docClient.get(rPolicy, doc1, "docBin", jsonPath);  //Using Read Filter Expression 
         if(obj != null) {
-          System.out.println("docClient Read Filter Expression (false): "+obj.toString());
+          System.out.println("Document Client Read Filter Expression [k2[0]>0] (true): "+obj.toString());
         } else {
-          System.out.println("docClient Read Filter Expression (false) returned null object");
+          System.out.println("Document Client Read Filter Expression [k2[0]>0] (true) returned null object");
         }
        
-        Record r = client.get(rPolicy, doc1);  //Using Read Filter Expression
-        System.out.println("client Read Filter Expression (false): "+ r);
+        r = client.get(rPolicy, doc1);  //Using Read Filter Expression
+        System.out.println("AerospikeClient Read Filter Expression [k2[0]>0 (true): "+ r);
+
+        long lut_01_01_2022 = 1641027600000000000L;
+        long lut_01_01_2032 = 1956560400000000000L;
+
+        //Read filter false on record metadata
+        rPolicy.filterExp = Exp.build( Exp.gt(Exp.lastUpdate(), Exp.val(lut_01_01_2032)));
+
+        obj = docClient.get(rPolicy, doc1, "docBin", jsonPath);  //Using Read Filter Expression 
+        if(obj != null) {
+          System.out.println("Document Client Read Filter Expression [lut>1/1/32] (false): "+obj.toString());
+        } else {
+          System.out.println("Document Client Read Filter Expression [lut>1/1/32] (false) returned null object");
+        }
+       
+        r = client.get(rPolicy, doc1);  //Using Read Filter Expression
+        System.out.println("AerospikeClient Read Filter Expression [lut>1/1/32 (false): "+ r);
+
+        //Read filter true on record metadata
+
+        rPolicy.filterExp = Exp.build( Exp.gt(Exp.lastUpdate(), Exp.val(lut_01_01_2022)));
+
+        obj = docClient.get(rPolicy, doc1, "docBin", jsonPath);  //Using Read Filter Expression 
+        if(obj != null) {
+          System.out.println("Document Client Read Filter Expression [lut>1/1/22] (true): "+obj.toString());
+        } else {
+          System.out.println("Document Client Read Filter Expression [lut>1/1/22] (true) returned null object");
+        }
+       
+        r = client.get(rPolicy, doc1);  //Using Read Filter Expression
+        System.out.println("AerospikeClient Read Filter Expression [lut>1/1/22 (true): "+ r);
+
+        //Write Filter Expression - insert another doc2bin with jsonNode
+        //with record data and record metadata filter
+
+        WritePolicy wPolicy = new WritePolicy();
+        //Write filter false 
+        wPolicy.filterExp = Exp.build( 
+              Exp.gt( 
+                ListExp.getByIndex(
+                    ListReturnType.VALUE, 
+                    Exp.Type.INT, 
+                    Exp.val(0), 
+                    Exp.mapBin("docBin"), 
+                    CTX.mapKey(Value.get("k2")) ),
+                Exp.val(2) 
+              )
+            );
+        docClient.put(wPolicy, doc1, "docRDataBin", jsonNode);  //Using Write Filter Expression 
+        System.out.println("Document API with Write Filter Expression [k2[0]>2 (false): "+ r);
+
+        //Write filter true
+        wPolicy.filterExp = Exp.build( 
+              Exp.gt( 
+                ListExp.getByIndex(
+                    ListReturnType.VALUE, 
+                    Exp.Type.INT, 
+                    Exp.val(0), 
+                    Exp.mapBin("docBin"), 
+                    CTX.mapKey(Value.get("k2")) ),
+                Exp.val(0) 
+              )
+            );
+        docClient.put(wPolicy, doc1, "docRDataBin", jsonNode);  //Using Write Filter Expression 
+        System.out.println("Document API with Write Filter Expression [k2[0]>0 (true): "+ r);
+
+        //Write filter false on record metadata
+        wPolicy.filterExp = Exp.build( Exp.gt(Exp.lastUpdate(), Exp.val(lut_01_01_2032)));
+
+        docClient.put(wPolicy, doc1, "docRMetaBin", jsonNode);  //Using Write Filter Expression 
+        r = client.get(null, doc1); //Read the record 
+        System.out.println("Document API with Write Filter Expression [lut>1/1/32 (false): "+ r);
+
+        //Write filter true on record metadata
+        wPolicy.filterExp = Exp.build( Exp.gt(Exp.lastUpdate(), Exp.val(lut_01_01_2022)));
+
+        docClient.put(wPolicy, doc1, "docRMetaBin", jsonNode);  //Using Write Filter Expression 
+        r = client.get(null, doc1);  //Read the record
+        System.out.println("Document API with Write Filter Expression [lut>1/1/22 (true): "+ r);
+
     }
 }
