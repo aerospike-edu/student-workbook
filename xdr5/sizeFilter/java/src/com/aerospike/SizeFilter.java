efa07abcb4f505ef7d64bc134edae25e905a1c3c
pygupta <pgupta@aerospike.com>
2022-02-05 10:28:51 -0800

    update size filter

--- xdr5/sizeFilter/java/src/com/aerospike/SizeFilter.java
+++ xdr5/sizeFilter/java/src/com/aerospike/SizeFilter.java
@@ -1,21 +1,14 @@
 package com.aerospike;
 
-import com.aerospike.client.AerospikeClient;
-import com.aerospike.client.AerospikeException;
-import com.aerospike.client.command.ParticleType;
 import com.aerospike.client.exp.Exp;
 import com.aerospike.client.exp.Expression;
 
 public class SizeFilter {
 
   public static void main(String[] args) {
-    AerospikeClient client = new AerospikeClient("127.0.0.1", 3000);
-
     Expression filter = Exp.build( Exp.ge(Exp.deviceSize(), Exp.val(127 * 1024)) );
     String sFilter = filter.getBase64();
     System.out.println("Filter in base64:"+ sFilter); //Prints: kwSRQc4AAfwA
-
-    client.close();
   }
 }
 
